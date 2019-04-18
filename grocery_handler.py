46cb94c4de4d4cebdddd3faf3b6baf84eb7fd106
Aline Abler <alinea@riseup.net>
2019-04-18 11:53:02 +0200

    Support multiple grocery lists

--- grocery_handler.py
+++ grocery_handler.py
@@ -10,15 +10,28 @@ def matches_message(message):
     l = message.lower()
     return l.startswith('buy ') \
            or l.startswith('grocer') \
+           or l.startswith('gift ') \
+           or l.startswith('gifts') \
+           or l.startswith('pack') \
            or l.startswith('shopping list')
 
 
 def handle(message, db, _):
     if message.lower().startswith('buy '):
-        return add_item(message[4:], db)
+        return add_item(message[4:], db, 'shopping')
     if message.lower().startswith('grocer') \
             or message.lower().startswith('shopping list'):
-        return grocery_list(db)
+        return grocery_list(db, 'shopping')
+    if message.lower().startswith('gift idea') \
+            or message.lower().startswith('gift list') \
+            or message.lower().startswith('gifts'):
+        return grocery_list(db, 'gifts')
+    elif message.lower().startswith('gift '):
+        return add_item(message[5:], db, 'gifts')
+    if message.lower().startswith('packing'):
+        return grocery_list(db, 'packing')
+    elif message.lower().startswith('pack '):
+        return add_item(message[5:], db, 'packing')
 
 
 def handle_button(data, db, _):
@@ -32,7 +45,7 @@ def handle_button(data, db, _):
         if not item:
             return "Something went wrong..."
         table.delete(id=item_id)
-        msg = grocery_list(db)
+        msg = grocery_list(db, item['list'])
         if not isinstance(msg, dict):
             msg = {
                 'message': msg,
@@ -42,20 +55,21 @@ def handle_button(data, db, _):
     return "Uh oh, something is off"
 
 
-def add_item(item, db):
+def add_item(item, db, list_type):
     item = " ".join(item.strip().split())
     table = db['groceries']
     new_item = {
         'name': item,
         'timestamp': datetime.datetime.now(),
+        'list': list_type,
     }
     table.insert(new_item)
-    return "{} was added to your shopping list.".format(item)
+    return "{} was added to your {} list.".format(item, list_type)
 
 
-def grocery_list(db):
+def grocery_list(db, list_type):
     table = db['groceries']
-    items = table.all()
+    items = table.find(list=list_type)
     buttons = []
 
     for item in items:
@@ -65,9 +79,9 @@ def grocery_list(db):
         }])
 
     if not buttons:
-        return "Your shopping list is empty."
+        return "Your {} list is empty.".format(list_type)
     return {
-        'message': "Your shopping list:",
+        'message': "Your {} list:".format(list_type),
         'buttons': buttons,
     }
 
