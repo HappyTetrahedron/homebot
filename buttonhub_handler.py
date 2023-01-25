0aa083af3fa5865f0fc006c95be14440f0e544e6
Aline Abler <alinea@riseup.net>
2023-01-26 00:03:15 +0100

    PoC buttonhub flow buttons

--- buttonhub_handler.py
+++ buttonhub_handler.py
@@ -5,11 +5,13 @@ import re
 import requests
 
 from utils import PERM_ADMIN
+from utils import get_affirmation
 
 key = "buttonhub"
 name = "Buttonhub"
 
 RUN_FLOW_REGEX = re.compile('^(?:run )?flow ([a-z0-9-]+)', flags=re.I)
+TRIGGER_FLOW = 'tr'
 
 params = {}
 
@@ -21,7 +23,7 @@ def help(permission):
         return
     return {
         'summary': "Can run flows on buttonhub",
-        'examples': ["Run flow fish-lamps-on"],
+        'examples': ["Run flow fish-lamps-on", "flow"],
     }
 
 
@@ -36,26 +38,72 @@ def setup(config, send_message):
 def matches_message(message):
     if not params['enabled']:
         return
-    return RUN_FLOW_REGEX.match(message) is not None
+    return RUN_FLOW_REGEX.match(message) is not None or message.lower().strip() == 'flow'
 
 
 def handle(message, **kwargs):
     if kwargs['permission'] < PERM_ADMIN:
         return "Sorry, you can't do this."
+    if message.lower().strip() == 'flow':
+        return prompt_flows()
     match = RUN_FLOW_REGEX.match(message)
     flow_name = match.groups()[0]
+    return trigger_flow(flow_name)
+
+
+def handle_button(data, **kwargs):
+    parts = data.split(':')
+    cmd = parts[0]
+
+    if cmd == TRIGGER_FLOW:
+        return trigger_flow(parts[1])
+
+    return "Uh oh, something is off"
+
+
+def trigger_flow(flow_name):
     url = '{}/flows/{}'.format(params['base_url'], flow_name)
     try:
         response = requests.post(url, timeout=5)
         response.raise_for_status()
         return {
-            'message': 'Flow triggered',
+            'message': 'Flow {} triggered'.format(flow_name),
+            'answer': get_affirmation(),
         }
     except HTTPError as e:
         return {
             'message': 'Failed to trigger flow: {}'.format(e),
+            'answer': 'Error!',
         }
     except requests.exceptions.ConnectionError:
         return {
             'message': 'Failed to trigger flow',
+            'answer': 'Error!',
         }
+
+
+def prompt_flows():
+    url = '{}/flows'.format(params['base_url'])
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        flows = response.json()['flows']
+        buttons = []
+        for flow in flows:
+            buttons.append([{
+                'text': flow,
+                'data': '{}:{}'.format(TRIGGER_FLOW, flow)
+            }])
+        return {
+            'message': "Which flow would you like to trigger?",
+            'buttons': buttons,
+        }
+    except HTTPError as e:
+        return {
+            'message': 'Failed to get flows: {}'.format(e),
+        }
+    except requests.exceptions.ConnectionError:
+        return {
+            'message': 'Failed to get flows',
+        }
+
